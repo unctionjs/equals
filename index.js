3a42b290263b474d77e03482c484fb22979cc39c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-import type from "@unction/type"
-import isEnumerable from "@unction/isenumerable"
-import isObject from "@unction/isobject"
-import isArray from "@unction/isarray"
-import length from "@unction/length"
-import deepEql from "deep-eql"
-
-import type {UnaryFunctionType} from "types"
-
-export default function equals (left: mixed): UnaryFunctionType {
-  return function equalsLeft (right: mixed): boolean {
+import type from "@unction/type";
+import isEnumerable from "@unction/isenumerable";
+import isObject from "@unction/isobject";
+import isArray from "@unction/isarray";
+import length from "@unction/length";
+import deepEql from "deep-eql";
+export default function equals(left) {
+  return function equalsLeft(right) {
     if (type(left) !== type(right)) {
-      return false
+      return false;
     }
 
     if (isEnumerable(left) && isEnumerable(right) && length(left) !== length(right)) {
-      return false
+      return false;
     }
 
     if (isObject(left) || isArray(left)) {
-      return deepEql(left, right)
+      return deepEql(left, right);
     }
 
-    return left === right
-  }
+    return left === right;
+  };
 }
